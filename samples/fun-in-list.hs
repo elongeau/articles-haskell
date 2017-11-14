fb623436d2d5fe81660ef68e306bbb9946a78a54
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-11-14 13:04:07 +0100

    fixup! Rewrite from scratch

--- samples/fun-in-list.hs
+++ samples/fun-in-list.hs
@@ -1,7 +1,9 @@
-listOfFunction :: [(String -> String)]
-listOfFunction = [
-    reverse, 
-    \s -> "Hello " ++ s
+-- une liste de fonction String -> String
+listOfFunction :: [String -> String]
+listOfFunction = 
+    [
+        reverse, -- inverse une String
+        \s -> "Hello " ++ s -- préfixe une String avec "Hello "
     ]
 
 run = map (\f -> f "world") listOfFunction
