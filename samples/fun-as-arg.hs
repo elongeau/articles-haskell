756562450fbfebd72e0ea4e6620cc4ae52e419d8
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-11-25 10:31:26 +0100

    fixup! Rewrite from scratch

--- samples/fun-as-arg.hs
+++ samples/fun-as-arg.hs
@@ -1,8 +1,12 @@
 repeatMe :: (Int -> String) -> Int -> IO ()
 repeatMe f count = do 
-    print (f count) -- <1>
+    -- f est la fonction passée en paramètre
+    -- on lui passe le compteur et on écrit le résultat sur la console
+    print (f count) 
     if count > 1
     then
-        repeatMe f (count - 1) -- <2>
+        -- on rappelle repeatMe avec les mêmes arguments sauf le compteur décrémenté
+        repeatMe f (count - 1) 
     else
-        return () -- <3>
+        -- si on a fini de répeter on s'arrête
+        return () 
