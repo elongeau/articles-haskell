5ed72f986edff7d9e0d6208ddee8ed23d66df05c
elongeau <emmanuel.longeau@gmail.com>
2017-12-05 20:47:33 +0100

    Correct after review (#11)

--- samples/fun-as-arg.hs
+++ samples/fun-as-arg.hs
@@ -1,12 +1,12 @@
 repeatMe :: (Int -> String) -> Int -> IO ()
-repeatMe f count = do 
-    -- f est la fonction passée en paramètre
-    -- on lui passe le compteur et on écrit le résultat sur la console
-    print (f count) 
+repeatMe f count = 
     if count > 1
     then
-        -- on rappelle repeatMe avec les mêmes arguments sauf le compteur décrémenté
-        repeatMe f (count - 1) 
+        do 
+            -- f est la fonction passée en paramètre
+            -- on lui passe le compteur et on écrit le résultat sur la console
+            print (f count) 
+            -- on rappelle repeatMe avec les mêmes arguments sauf le compteur décrémenté
+            repeatMe f (count - 1) 
     else
-        -- si on a fini de répeter on s'arrête
-        return () 
+        print "Done !"
--- samples/pure.hs
+++ samples/pure.hs
@@ -1,16 +1,16 @@
--- pure 
+-- pur 
 add :: Int -> Int -> Int
 add x y = 
     x + y
 
--- pure : grâce à la transparence référentielle (et l'immuabilité de i)
+-- pur : grâce à la transparence référentielle (et l'immuabilité de i)
 i = 42
 
 answer :: String -> String
 answer question = 
     question ++ " is " ++ show i
 
--- pas pure : on lit depuis la console puis on y écrit
+-- pas pur : on lit depuis la console puis on y écrit
 greetings :: IO ()
 greetings =
     do 
