52837c30d81e5d2068858e0cc258e63292b40063
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-12-15 15:46:33 +0100

    fixup! Reorganize all article

--- samples/test.hs
+++ samples/test.hs
@@ -2,24 +2,20 @@ type Lazy a = () -> a
 type UnitTest =  Lazy (Bool, String) -- <1>
 
 -- une fonction pour construire des UnitTests
-test :: String -> (() -> Bool) -> UnitTest 
+test :: String -> (() -> Bool) -> UnitTest -- <2>
 test label testFun =
     -- comme un UnitTest est une fonction on va retourner une lambda
     -- l'intérêt est de laisser le framework de test décider quand éxécuter le test
-    \() -> 
-        let -- <2>
+    \() ->
+        let
             -- on éxécute le test
             testResult = testFun ()
         in
             -- on renvoi une paire (<résultat du test>, <description du test>)
             (testResult, label)
 
--- exécute des tests
-runTests :: [UnitTest] -> [(Bool,String)]
-runTests = map (\test -> test ())
-
 -- interprète le résultat d'un test
-interpret :: Bool -> String -- <2> la rédaction décline tout responsabilité en cas de dépendance au pattern matching
+interpret :: Bool -> String -- <3>
 interpret True = "✔"
 interpret False = "✘"
         
@@ -41,15 +37,17 @@ format (result, label) =
 describe :: String -> [UnitTest] -> String
 describe label tests =
     let
+        runTests = map (\test -> test ()) -- <4>
         -- exécute tous les test puis formate leur résultat
-        runAll = map format . runTests $ tests
+        runAll = map format . runTests $ tests -- <5>
         -- ajoute le descriptif tu groupe
-        report = label : runAll
+        report = label : runAll -- <6>
     in
         -- formate le tout
         unlines report        
 
         
+-- On lance nos tests
 main = 
     putStrLn $ describe "addition" [
         test "1 + 1 == 2" (\() -> 1 + 1 == 2),
